1651aed273ce385899d3bf1269fbd87f276cd4f7
Parham Alvani <parham.alvani@gmail.com>
2016-03-17 23:15:10 +0330

    Runnable script was added.

---
+++ scripts/kaa.py
@@ -0,0 +1,11 @@
+# In The Name Of God
+# ========================================
+# [] File Name : kaa.py
+#
+# [] Creation Date : 3/17/16
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+from cli.cli import KaaCLICmd
+
+KaaCLICmd().cmdloop()
