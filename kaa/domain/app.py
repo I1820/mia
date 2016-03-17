aa84d226c2f870c2cbeb80017911e7950b1eb357
Parham Alvani <parham.alvani@gmail.com>
2016-03-17 22:15:56 +0330

    Kaa application representation was added.

---
+++ kaa/domain/app.py
@@ -0,0 +1,49 @@
+# In The Name Of God
+# ========================================
+# [] File Name : app.py
+#
+# [] Creation Date : 3/17/16
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+import json
+
+
+class Application:
+    """
+    This object represents a Kaa application
+    :type application_token: str
+    :type app_id: str
+    :type name: str
+    :type sequence_number: int
+    :type tenant_id: str
+    """
+
+    def __init__(self, application_token, app_id, name, sequence_number, tenant_id):
+        self.application_token = application_token
+        self.id = app_id
+        self.name = name
+        self.sequence_number = sequence_number
+        self.tenant_id = tenant_id
+
+
+class GroupChatJSONEncoder(json.JSONEncoder):
+    def default(self, obj: Application):
+        if obj is Application:
+            return {
+                'application_token': obj.application_token,
+                'id': obj.id,
+                'name': obj.name,
+                'sequence_number': obj.sequence_number,
+                'tenant_id': obj.tenant_id
+            }
+        else:
+            raise TypeError("UserJsonEncoder got {} instead of Application.".format(type(obj)))
+
+
+class GroupChatDictDecoder:
+    @staticmethod
+    def decode(obj: dict) -> Application:
+        return Application(application_token=obj['application_token'], app_id=obj['id'], name=obj['name'],
+                           sequence_number=int(obj['sequence_number']), tenant_id=obj['tenant_id'])
