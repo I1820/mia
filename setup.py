5862619e08ff010620d2a0795b9140ae385fb5c5
Parham Alvani <parham.alvani@gmail.com>
2016-04-01 20:42:18 +0430

    Nothing important ..

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description_text = f.read()
+
+# Get the long description from the README file
+with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
+    license_text = f.read()
 
 setup(
     name='Kaa.py',
-    version='0.5.dev1',
-    packages=['kaa', 'kaa.cli', 'kaa.rest', 'kaa.domain'],
+    version='0.5.dev2',
+    packages=find_packages(),
     url='https://github.com/AoLab/Kaa.py',
-    license='LICENSE',
+    license=license_text,
+    long_description=long_description_text,
     author='Parham Alvani',
     author_email='parham.alvani@gmail.com',
     description='Kaa Administration REST Client',
