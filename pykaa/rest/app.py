21161e72fd4ad85bd77353b9e46f941e2605691d
Parham Alvani <parham.alvani@gmail.com>
2016-04-01 23:44:49 +0430

    REST modules were improved.

--- pykaa/rest/app.py
+++ pykaa/rest/app.py
@@ -7,22 +7,59 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 
-from .base import KaaRestBase
+from .base import KaaRestBase, KaaRestError
 from pykaa.domain.app import Application, ApplicationDictDecoder
 
 import requests
+from requests.exceptions import HTTPError
 import json
 
 
 class KaaRestApplication(KaaRestBase):
+    """
+    This class provide a rest way for interfacing with kaa applications
+    """
+
     def __int__(self, address, username, password):
         super(KaaRestApplication, self).__init__(address, username, password)
 
     def get_all_applications(self) -> [Application]:
         applications = []
         response = requests.get(self.url_prefix + "applications")
+        try:
+            response.raise_for_status()
+        except HTTPError as e:
+            if response.status_code == 401:
+                raise KaaRestApplicationError(
+                    "The user is not authenticated or"
+                    " invalid credentials were provided", e) from e
+            if response.status_code == 403:
+                raise KaaRestApplicationError(
+                    "The authenticated user does not have the required role"
+                    " (TENANT_ADMIN, TENANT_DEVELOPER, or TENANT_USER)", e) \
+                    from e
+            if response.status_code == 500:
+                raise KaaRestApplicationError(
+                    "An unexpected error occurred on the server side", e) \
+                    from e
         response = json.loads(response.text)
         for obj in response:
             application = ApplicationDictDecoder.decode(obj)
             applications.append(application)
         return applications
+
+    def get_application_by_id(self, application_id: str) -> Application:
+        response = requests.get(
+            self.url_prefix + "application/{}".format(application_id))
+        response = json.loads(response.text)
+        application = ApplicationDictDecoder.decode(response)
+        return application
+
+
+class KaaRestApplicationError(KaaRestError):
+    """
+    This class wraps server HTTP errors for kaa applications
+    """
+
+    def __init__(self, message, exception):
+        super(KaaRestApplicationError, self).__init__(message, exception)
--- pykaa/rest/base.py
+++ pykaa/rest/base.py
@@ -7,6 +7,8 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 
+from requests.exceptions import HTTPError
+
 
 class KaaRestBase:
     """
@@ -19,4 +21,22 @@ class KaaRestBase:
     def __init__(self, address, username, password):
         self.username = username
         self.password = password
-        self.url_prefix = 'http://{0}:{1}@{2}/kaaAdmin/rest/api/'.format(username, password, address)
+        self.url_prefix = 'http://{0}:{1}@{2}/kaaAdmin/rest/api/'.format(
+            username, password, address)
+
+
+class KaaRestError(Exception):
+    """
+    This class provide basis for all Kaa Rest modules errors
+
+    :type message: str
+    :type exception: HTTPError
+    """
+
+    def __init__(self, message, exception):
+        super(KaaRestError, self).__init__()
+        self.message = message
+        self.exception = exception
+
+    def __str__(self) -> str:
+        return "{0}: {1}".format(self.exception, self.message)
