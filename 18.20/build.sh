b7355f221cfc8b2ced40ebd9a79e6dd47275c664
Parham Alvani <parham.alvani@gmail.com>
2016-08-01 23:11:12 +0430

    Build script was added.

---
+++ 18.20/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# In The Name Of God
+# ========================================
+# [] File Name : build.sh
+#
+# [] Creation Date : 01-08-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+docker build -t aolab/18.20 .
