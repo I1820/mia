c70dab19c4ab72577e6c39379a4b62961fd6a5ee
Parham Alvani <parham.alvani@gmail.com>
2016-08-26 19:01:42 +0430

    Main http server file was added.

---
+++ I1820/http/main.py
@@ -0,0 +1,11 @@
+# In The Name Of God
+# ========================================
+# [] File Name : main.py
+#
+# [] Creation Date : 26-08-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+from .route import app
+
+app.run(debug=True, host="0.0.0.0")
