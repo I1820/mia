2af0c5ea4063aba3e3905a1f17e48001d2a61dd4
Parham Alvani <parham.alvani@gmail.com>
2016-08-26 09:06:17 +0430

    Improve get all sdks method with new kaa :)

--- pykaa/rest/sdk.py
+++ pykaa/rest/sdk.py
@@ -20,10 +20,10 @@ class KaaRestSDKProfile(KaaRestBase):
     def __init__(self, address, username, password):
         super(KaaRestSDKProfile, self).__init__(address, username, password)
 
-    def get_all_sdk_profiles(self, application_id: int) -> [SDKProfile]:
+    def get_all_sdk_profiles(self, application_token: sdk) -> [SDKProfile]:
         sdk_profiles = []
         response = requests.get(
-            self.url_prefix + "sdkProfiles/{}".format(application_id))
+            self.url_prefix + "sdkProfilesByAppToken/{}".format(application_token))
         response = json.loads(response.text)
         for obj in response:
             sdk_profile = SDKProfileDictDecoder.decode(obj)
