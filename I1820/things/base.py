8d81a4a4df6402cd917a818cd7f71d9eb7a40196
Parham Alvani <parham.alvani@gmail.com>
2016-08-26 19:43:02 +0430

    Things basis were added.

---
+++ I1820/things/base.py
@@ -0,0 +1,34 @@
+# In The Name Of God
+# ========================================
+# [] File Name : base.py
+#
+# [] Creation Date : 26-08-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+import abc
+import importlib
+
+
+class Things(abc.ABCMeta):
+    things = {}
+
+    def __new__(metaclass, name, bases, namespace):
+        cls = abc.ABCMeta.__new__(
+            metaclass, name, bases, namespace)
+        if isinstance(cls.name, str):
+            metaclass.things[cls.name] = cls
+        return cls
+
+    @classmethod
+    def get(cls, name):
+        if name not in cls.things:
+            importlib.import_module('I1820.things.%s' % name)
+        return cls.things[name]
+
+
+class Thing(metaclass=Things):
+    @property
+    @abc.abstractmethod
+    def name(self):
+        raise NotImplemented()
