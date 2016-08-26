47cf349c4234e28bc5e5f74564fb404c31dc1a25
Parham Alvani <parham.alvani@gmail.com>
2016-08-26 22:25:21 +0430

    Import error was handled :)

--- I1820/http/route.py
+++ I1820/http/route.py
@@ -6,15 +6,18 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-from flask import request
+import flask
 
 from . import app
 from ..things.base import Things
 
 
-@app.route('/<uuid:rpi_id>/<uuid:device_id>', methods=['POST', 'PUT'])
+@app.route('/thing/<uuid:rpi_id>/<uuid:device_id>', methods=['POST', 'PUT'])
 def thing_handler(rpi_id, device_id):
-    data = request.get_json(force=True)
+    data = flask.request.get_json(force=True)
     data['rpi_id'] = rpi_id
     data['device_id'] = device_id
-    return Things.get(data['type']).handle(data)
+    try:
+        return Things.get(data['type']).handle(data)
+    except ImportError:
+        flask.abort(400)
