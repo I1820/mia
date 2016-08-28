51040e1c9df53b04cc094367c4f41f43fba1dfac
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-29 00:09:06 +0430

    Job is done!

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.text());
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+
+
+var lastSocket;
+app.post('/api', function (req, res) {
+    console.log("Post request to /api");
+    io.emit('chat message', req.body);
+    console.log("Post request to /api");
+    res.send('Success');
+});
+
+
 io.on('connection', function(socket){
     console.log('a user connected');
+    lastSocket = socket;
     socket.on('chat message', function(msg){
         console.log(msg);
     });
-    io.emit('chat message', "Salam");
-});
 
+});
 
 
 
