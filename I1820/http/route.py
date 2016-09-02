90da9baf969f2c23f31b3ee6e790a17f9de12a99
Parham Alvani <parham.alvani@gmail.com>
2016-09-02 22:48:18 +0430

    Improve things handling in http :)

--- I1820/http/route.py
+++ I1820/http/route.py
@@ -7,6 +7,7 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 import flask
+import json
 
 from . import app
 from ..things.base import Things
@@ -17,13 +18,26 @@ def test_handler():
     return "18.20 is leaving us"
 
 
+@app.route('/service/<string:name>', methods=['POST'])
+def service_handler(name):
+    pass
+
+
 @app.route('/thing/<uuid:rpi_id>/<uuid:device_id>', methods=['POST', 'PUT'])
 def thing_handler(rpi_id, device_id):
     data = flask.request.get_json(force=True)
-    data['rpi_id'] = rpi_id
-    data['device_id'] = device_id
+    result = {}
     try:
-        return Things.get(data['type']).handle(data)
+        thing = Things.get(data['type'])(rpi_id, device_id)
     except ImportError as e:
         return ('%s is not one of our things: %s' % (data['type'], str(e)),
                 400, {})
+    if 'settings' in data.keys():
+        for key, value in data['settings'].items():
+            setattr(thing, key, value)
+            result[key] = value
+    if 'status' in data.keys():
+        for key in data['status']:
+            result[key] = getattr(thing, key)
+
+    return json.dumps(result)
