39951aff142e337386c510c599caa87746678fb1
Parham Alvani <parham.alvani@gmail.com>
2016-09-04 00:00:54 +0430

    Log module was added.

---
+++ I1820/log/log.py
@@ -0,0 +1,10 @@
+# In The Name Of God
+# ========================================
+# [] File Name : log.py
+#
+# [] Creation Date : 04-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+
