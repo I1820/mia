06ab0b0433f94fee186ba0f6d597afe54e409607
Parham Alvani <parham.alvani@gmail.com>
2016-09-04 21:03:42 +0430

    I1820Logger class was improved.

--- I1820/log/log.py
+++ I1820/log/log.py
@@ -12,11 +12,16 @@ import datetime
 class I1820Logger:
     def __init__(self, timestamp: datetime.datetime,
                  data: dict, endpoint: str):
-        pass
+        self._status = data['status']
+        for key, value in data['status'].items():
+            setattr(self, key, value)
+        self.timestamp = timestamp
+        self.endpoint = endpoint
 
     def save(self):
-        pass
+        for key, value in self._status.items():
+            pass
 
     @classmethod
-    def last(cls):
+    def last(cls, me):
         pass
