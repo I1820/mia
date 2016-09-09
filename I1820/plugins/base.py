ab1c4533d8d506d47b3a43cd979fe9f53d0b7d1c
Parham Alvani <parham.alvani@gmail.com>
2016-09-09 21:20:43 +0430

    I1820 have plugins :D

--- I1820/plugins/base.py
+++ I1820/plugins/base.py
@@ -10,18 +10,25 @@ import abc
 import importlib
 
 
-class Services(abc.ABCMeta):
-    services = {}
+class Plugins(abc.ABCMeta):
+    plugins = {}
 
     def __new__(metaclass, name, bases, namespace):
         cls = abc.ABCMeta.__new__(
             metaclass, name, bases, namespace)
         if isinstance(cls.name, str):
-            metaclass.services[cls.name] = cls
+            metaclass.plugins[cls.name] = cls()
         return cls
 
     @classmethod
     def get(cls, name):
-        if name not in cls.things:
+        if name not in cls.services.keys():
             importlib.import_module('I1820.services.%s' % name)
-        return cls.things[name]
+        return cls.plugins[name]
+
+
+class Plugin(metaclass=Plugins):
+    @property
+    @abc.abstractmethod
+    def name(self):
+        raise NotImplemented()
