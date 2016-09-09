e1f6815e750e14a826596dc29770e3ae24a28d2c
Parham Alvani <parham.alvani@gmail.com>
2016-09-09 19:21:28 +0430

    Index javascript file was added.

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 09-09-2016
+ * |
+ * | File Name:     index.js
+ * +===============================================
+ */
+window.onLoad = onIndexLoad
+
+function onIndexLoad () {
+  var ws = new WebSocket()
+}
