710453777700f99508ed535e4b2bc6c77f921f60
Parham Alvani <parham.alvani@gmail.com>
2016-09-10 01:25:00 +0430

    Index javascript was improved.

--- index.js
+++ index.js
@@ -9,6 +9,20 @@
  */
 window.onLoad = onIndexLoad
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    messages: [{
+      id: '123',
+      ip: '192.168.12.2',
+      humidity: 25
+    }]
+  }
+})
+
 function onIndexLoad () {
-  var ws = new WebSocket()
+  var ws = new WebSocket('ws://www.example.com/socketserver')
+  ws.onmessage = function (message) {
+    app.message.push(message.data)
+  }
 }
