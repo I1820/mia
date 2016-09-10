0f1ed3b820d0cd8dcb52fb1b9b12db5e15c9ee0e
Parham Alvani <parham.alvani@gmail.com>
2016-09-10 21:58:51 +0430

    SensorThing class was added.

---
+++ I1820/things/sensor.py
@@ -0,0 +1,13 @@
+# In The Name Of God
+# ========================================
+# [] File Name : sensor.py
+#
+# [] Creation Date : 07-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+from .base import Thing
+
+
+class SensorThing(Thing):
+    pass
