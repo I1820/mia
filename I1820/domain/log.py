50d4a623e10f82992e0b8e8ffa6558161fd01bf5
Parham Alvani <parham.alvani@gmail.com>
2016-09-10 20:32:58 +0430

    Log model was added.

---
+++ I1820/domain/log.py
@@ -0,0 +1,38 @@
+# In The Name Of God
+# ========================================
+# [] File Name : log.py
+#
+# [] Creation Date : 04-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+import datetime
+import json
+
+
+class I1820Log:
+    def __init__(self, type: str, device: str,
+                 states: dict,
+                 endpoint: str,
+                 timestamp: datetime.datetime = datetime.datetime.now()):
+        self.states = states
+        self.type = type
+        self.device = device
+        self.timestamp = timestamp
+        self.endpoint = endpoint
+
+
+class I1820LogJSONEncoder(json.JSONEncoder):
+    def default(self, obj: I1820Log):
+        if isinstance(obj, I1820Log):
+            return {
+                'timestamp': obj.timestamp.timestamp(),
+                'type': obj.type,
+                'device': obj.device,
+                'data': {'states': obj.states},
+                'endpoint': obj.endpoint
+            }
+        else:
+            raise TypeError(
+                "ApplicationJsonEncoder got {} instead of I1820Log.".format(
+                    type(obj)))
