2bb3ae79e0f6413207a2da6d0a51e4a153b67b45
Parham Alvani <parham.alvani@gmail.com>
2016-09-13 01:16:09 +0430

    Notification controller was added.

---
+++ I1820/controller/notif.py
@@ -0,0 +1,22 @@
+# In The Name Of God
+# ========================================
+# [] File Name : notif.py
+#
+# [] Creation Date : 13-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+import requests
+
+from .base import I1820Controller
+from ..domain.notif import I1820Notification
+from .discovery import DiscoveryController
+
+
+class NotificationController(I1820Controller):
+    def __init__(self):
+        pass
+
+    def notify(self, notification: I1820Notification):
+        ip = DiscoveryController.rpis[notification['endpoint']]['ip']
+        requests.post('http://%s:1373/event' % ip)
