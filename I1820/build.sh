16beea55500c81c0fe1173b3c59f53cfd040bf69
Parham Alvani <parham.alvani@gmail.com>
2016-09-14 02:02:58 +0430

    I1820 docker files were added.

---
+++ I1820/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# In The Name Of God
+# ========================================
+# [] File Name : build.sh
+#
+# [] Creation Date : 01-08-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+docker build --force-rm -t lolab/i1820 .
