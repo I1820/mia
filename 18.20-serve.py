a9ee3e883d4dee04d4616193adb525f724727b20
Parham Alvani <parham.alvani@gmail.com>
2016-09-14 17:22:06 +0430

    Improve server runner script :)

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-from I1820.http.main import main
+from multiprocessing import Process
+
+from I1820.http.main import main as http_main
+from I1820.ws.main import main as ws_main
+
 
 if __name__ == '__main__':
-    main()
+    Process(target=ws_main).start()
+    http_main()
