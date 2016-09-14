c4442e950c0d0625970d14bb42562c0f06b680c6
Parham Alvani <parham.alvani@gmail.com>
2016-09-14 11:26:21 +0430

    Empty main was added for websocket.

---
+++ I1820/ws/main.py
@@ -0,0 +1,8 @@
+# In The Name Of God
+# ========================================
+# [] File Name : main.py
+#
+# [] Creation Date : 14-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
