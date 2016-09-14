6c626b22331ebf95deb50f90c1c77374ebdfe4ab
Parham Alvani <parham.alvani@gmail.com>
2016-09-15 00:50:03 +0430

    I1820 server runner script was improved.

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
-from multiprocessing import Process
-
-from I1820.http.main import main as http_main
-from I1820.ws.main import main as ws_main
+from I1820.http.main import main
 
 
 if __name__ == '__main__':
-    Process(target=ws_main).start()
-    http_main()
+    main()
