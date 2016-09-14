67c51fba42d43dcd595bcb37ffba9acdacb2ff11
Parham Alvani <parham.alvani@gmail.com>
2016-09-14 16:11:01 +0430

    Websocket main was added.

--- I1820/ws/main.py
+++ I1820/ws/main.py
@@ -6,3 +6,13 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+import websockets
+import asyncio
+
+from .handler import handler
+
+
+def main():
+    start_server = websockets.serve(handler, '0.0.0.0', 8765)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
