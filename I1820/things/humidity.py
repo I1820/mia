37d0f1953315dc33481468face5286bffa9ade10
Parham Alvani <parham.alvani@gmail.com>
2016-09-19 14:29:38 +0430

    Humidity was added.

---
+++ I1820/things/humidity.py
@@ -0,0 +1,23 @@
+# In The Name Of God
+# ========================================
+# [] File Name : humidity.py
+#
+# [] Creation Date : 10-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+from .sensor import SensorThing
+
+
+class Humidity(SensorThing):
+    """
+    This class represents Temperature sensor
+    """
+    name = "humidity"
+
+    def __init__(self, rpi_id, device_id):
+        super().__init__(rpi_id, device_id)
+
+    @property
+    def allowed_states(self):
+        return ['humidity']
