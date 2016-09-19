ee4a93491ad6c75de7f53c980f34ae5377fee979
Parham Alvani <parham.alvani@gmail.com>
2016-09-20 01:26:02 +0430

    Sensor can log the data now :)

--- I1820/things/sensor.py
+++ I1820/things/sensor.py
@@ -13,7 +13,6 @@ from ..controller.log import LogController
 
 
 class SensorThing(Thing):
-    # TODO: Provide log analyzer
 
     def __init__(self, rpi_id, device_id):
         super().__init__(rpi_id, device_id)
@@ -25,7 +24,13 @@ class SensorThing(Thing):
 
     def __setattr__(self, name, value):
         if name in self.allowed_states:
-            raise ValueError('Sensor states are not writeable')
+            if isinstance(value, dict):
+                LogController().save(name, self.name, self.rpi_id,
+                                     self.device_id,
+                                     value['time'], value['value'])
+            else:
+                raise ValueError(
+                    'Sensor states are not writeable by %s' % type(value))
         super().__setattr__(name, value)
 
     def __getattr__(self, name):
