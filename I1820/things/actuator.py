051564515fa1a4da1cc7e92f81e316ce3d4115ab
Parham Alvani <parham.alvani@gmail.com>
2016-09-19 18:19:30 +0430

    Move common functionalities into actuator class.

--- I1820/things/actuator.py
+++ I1820/things/actuator.py
@@ -6,9 +6,30 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+import abc
+
 from .base import Thing
+from ..domain import I1820Notification
+from ..controller import NotificationController
 
 
 class ActuatorThing(Thing):
     def __init__(self, rpi_id, device_id):
         super().__init__(rpi_id, device_id)
+
+    @property
+    @abc.abstractmethod
+    def allowed_settings(self):
+        raise NotImplemented()
+
+    def __setattr__(self, name, value):
+        if name in self.allowed_settings:
+            message = I1820Notification(self.name, self.device_id,
+                                        {name: value}, self.rpi_id)
+            NotificationController().notify(message)
+        super().__setattr__(name, value)
+
+    def __getattr__(self, name):
+        if name in self.allowed_settings:
+            raise ValueError('Actuator settings are not readable')
+        return super().__getattr__(name)
--- I1820/things/lamp.py
+++ I1820/things/lamp.py
@@ -7,28 +7,18 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 from .actuator import ActuatorThing
-from ..domain import I1820Notification
-from ..controller import NotificationController
 
 
 class Lamp(ActuatorThing):
     """
     This class represents Lamp actuator
     """
+
     name = "lamp"
 
     def __init__(self, rpi_id, device_id):
         super().__init__(rpi_id, device_id)
 
     @property
-    def on(self):
-        """
-        On property is used as a trigger for lamp status.
-        """
-        raise ValueError('Actuator settings are not readable')
-
-    @on.setter
-    def on(self, on: bool):
-        message = I1820Notification('lamp', self.device_id, {'on': on},
-                                    self.rpi_id)
-        NotificationController().notify(message)
+    def allowed_settings(self):
+        return ['on']
