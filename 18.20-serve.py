309446cd24d76d06c4c807ab29fdd2a039236129
Parham Alvani <parham.alvani@gmail.com>
2016-09-22 02:01:41 +0330

    Create thread for coap and flask.

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-from multiprocessing import Process
+import threading
 
 from I1820.http.main import main as http_main
 from I1820.coap.main import main as coap_main
 
 
 if __name__ == '__main__':
-    p = Process(target=coap_main)
-    p.start()
-    http_main()
+    http_thread = threading.Thread(target=http_main)
+    coap_thread = threading.Thread(target=coap_main)
+    http_thread.start()
+    coap_thread.start()
