f0c3f15da48bfc13048a97a83b845d923f7e8291
Parham Alvani <parham.alvani@gmail.com>
2016-09-22 23:42:47 +0330

    Provide up and running script :)

--- I1820/up-running.sh
+++ I1820/up-running.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # In The Name Of God
 # ========================================
-# [] File Name : build.sh
+# [] File Name : up-running.sh
 #
 # [] Creation Date : 01-08-2016
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 docker build --force-rm -t aolab/i1820 .
+docker run -p 8080:8080 --name="I1820" aolab/i1820
+docker rm "I1820"
