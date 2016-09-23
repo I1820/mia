b7e38b0e27868da3ae5a3e690d4c4e2f7c1ae303
Iman Tabrizian <tabrizian@outlook.com>
2016-09-23 15:28:42 +0330

    Remove one remaining sudo

--- scripts/influxdb-install.sh
+++ scripts/influxdb-install.sh
@@ -9,6 +9,6 @@
 # =======================================
 curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
 source /etc/lsb-release
-echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
 apt-get update && apt-get install influxdb
 service influxdb start
