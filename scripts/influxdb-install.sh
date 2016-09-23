27945cbf1e6735c19e69556bbf566c63d50245ee
Iman Tabrizian <tabrizian@outlook.com>
2016-09-23 15:18:54 +0330

    RUn whole command as sudo not individual ones

--- scripts/influxdb-install.sh
+++ scripts/influxdb-install.sh
@@ -7,8 +7,8 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-sudo apt-get update && sudo apt-get install influxdb
-sudo service influxdb start
+apt-get update && apt-get install influxdb
+service influxdb start
