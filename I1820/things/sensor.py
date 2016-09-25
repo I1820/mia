68d119dd590b037f7bcbbca95155b474e48ef22c
Parham Alvani <parham.alvani@gmail.com>
2016-09-26 01:58:55 +0330

    Events were added for sensor things.

--- I1820/things/sensor.py
+++ I1820/things/sensor.py
@@ -12,15 +12,37 @@ from .base import Thing
 from ..controller.log import LogController
 
 
+def make_event_setter(event):
+    def event_setter(self, value):
+        if isinstance(value, dict):
+            setattr(self, "_%s" % event, value['time'])
+    return event_setter
+
+
+def make_event_getter(event):
+    def event_getter(self):
+        time = getattr(self, "_%s" % event)
+        return time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return event_getter
+
+
 class SensorThing(Thing):
 
     def __init__(self, rpi_id, device_id):
         super().__init__(rpi_id, device_id)
+        for allowed_event in self.allowed_events:
+            setattr(type(self), allowed_event,
+                    property(make_event_getter(allowed_event),
+                             make_event_setter(allowed_event), None))
 
     @property
     @abc.abstractmethod
     def allowed_states(self):
         raise NotImplemented()
+
+    @property
+    def allowed_events(self):
+        return []
 
     def __setattr__(self, name, value):
         if name in self.allowed_states:
