116372b1b6f43a5a1b472f84340206c2d950a204
Parham Alvani <parham.alvani@gmail.com>
2016-10-07 23:44:58 +0330

    Discovery controller was improved into lock free design :)

--- I1820/controller/discovery.py
+++ I1820/controller/discovery.py
@@ -10,27 +10,26 @@ from datetime import datetime
 from .base import I1820Controller
 from ..things.base import Things
 
-import threading
-
 
 class DiscoveryController(I1820Controller):
     def __init__(self):
-        self._rpis = dict()
-        self.lock = threading.Lock()
+        self.rpis = dict()
 
     def ping(self, message: dict, ip: str):
-        self.rpis[message['rpi_id']] = {
-            'time': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-            'ip': ip,
-            'things': message['things']
-        }
+        if message['rpi_id'] not in self.rpis:
+            self.rpis[message['rpi_id']] = {
+                'time': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+                'ip': ip,
+                'things': []
+            }
+        else:
+            self.rpis[message['rpi_id']]['time'] = \
+                    datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+            self.rpis[message['rpi_id']]['ip'] = ip
+
         for thing in message['things']:
             if not Things.get(thing['type']).has_thing(message['rpi_id'],
                                                        thing['id']):
                 Things.get(thing['type']).new_thing(
                     message['rpi_id'], thing['id'])
-
-    @property
-    def rpis(self):
-        with self.lock:
-            return self._rpis
+                self.rpis[message['rpi_id']]['things'].append(thing)
