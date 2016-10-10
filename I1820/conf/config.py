d4d8e356d3f0c6d068921f44f67fe955affbb07d
Parham Alvani <parham.alvani@gmail.com>
2016-10-11 01:56:34 +0330

    Uses python default configuration instead of YML.

--- I1820/conf/config.py
+++ I1820/conf/config.py
@@ -6,31 +6,31 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-import yaml
 import os
+import configparser
 
 
 class I1820Config:
     def __init__(self, path):
-        try:
-            ymlfile = open(path, 'r')
-        except IOError:
-            cfg = {}
-            cfg['influxdb'] = {}
-            cfg['influxdb']['host'] = os.getenv(
-                'I1820_INFLUXDB_HOST', '0.0.0.0')
-            cfg['influxdb']['port'] = os.getenv(
-                'I1820_INFLUXDB_PORT', '8086')
-            cfg['influxdb']['db'] = os.getenv(
-                'I1820_INFLUXDB_DB', 'I1820')
-            cfg['influxdb']['user'] = os.getenv(
-                'I1820_INFLUXDB_USER', 'root')
-            cfg['influxdb']['passwd'] = os.getenv(
-                'I1820_INFLUXDB_PASSWD', 'root')
-            cfg['endpoints'] = os.getenv('I1820_ENDPOINTS', '').split(' ')
-        else:
-            with ymlfile:
-                cfg = yaml.load(ymlfile)
+        cfg = configparser.ConfigParser()
+
+        # Environment based configurations
+        env_cfg = {}
+        env_cfg['InfluxDB'] = {}
+        env_cfg['InfluxDB']['host'] = os.getenv('I1820_INFLUXDB_HOST',
+                                                '0.0.0.0')
+        env_cfg['InfluxDB']['port'] = os.getenv('I1820_INFLUXDB_PORT', '8086')
+        env_cfg['InfluxDB']['db'] = os.getenv('I1820_INFLUXDB_DB', 'I1820')
+        env_cfg['InfluxDB']['user'] = os.getenv('I1820_INFLUXDB_USER', 'root')
+        env_cfg['InfluxDB']['passwd'] = os.getenv('I1820_INFLUXDB_PASSWD',
+                                                  'root')
+        env_cfg['Things'] = {}
+        env_cfg['Things']['endpoints'] = os.getenv('I1820_ENDPOINTS', '')
+        cfg.read_dict(env_cfg)
+
+        # File based configurations
+        cfg.read(path)
+
         self.cfg = cfg
 
     def __getattr__(self, name):
@@ -39,10 +39,10 @@ class I1820Config:
         else:
             section = name
         if section == 'influxdb':
-            return self.cfg[section][field]
+            return self.cfg['InfluxDB'][field]
         elif section == 'endpoints':
-            return self.cfg[section]
+            return self.cfg['Things']['endpoints'].split(' ')
 
 
-I1820_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "1820.yml")
+I1820_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "1820.ini")
 cfg = I1820Config(I1820_CONFIG_PATH)
