33058d0d6950f1d5b3a6ee74b3857be304183850
Parham Alvani <parham.alvani@gmail.com>
2016-10-13 02:13:47 +0330

    Tries to remove socket IO.

--- I1820/http/__init__.py
+++ I1820/http/__init__.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from flask_socketio import SocketIO
 
 
 app = Flask("I1820")
-socketio = SocketIO(app)
--- I1820/http/main.py
+++ I1820/http/main.py
@@ -7,8 +7,7 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 from .route import app
-from .route import socketio
 
 
 def main():
-    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
