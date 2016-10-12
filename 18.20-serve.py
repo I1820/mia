735e8060e0eb5110d9878970491d4d32b9203db8
Parham Alvani <parham.alvani@gmail.com>
2016-10-13 03:22:52 +0330

    I1820 at your service :D

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from I1820.http.main import main
+from I1820.http.main import main as main_http
+from I1820.mqtt.main import main as main_mqtt
 
 if __name__ == '__main__':
-    main()
+    main_mqtt()
+    main_http()
