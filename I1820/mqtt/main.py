f709c1d81f574787abe64557b55627298b887791
Parham Alvani <parham.alvani@gmail.com>
2016-10-13 01:41:25 +0330

    Main of MQTT was added.

---
+++ I1820/mqtt/main.py
@@ -0,0 +1,13 @@
+# In The Name Of God
+# ========================================
+# [] File Name : main.py
+#
+# [] Creation Date : 12-10-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+from .route import client
+
+
+def main():
+    client.loop_start()
