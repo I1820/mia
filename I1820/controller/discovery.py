8d097ba40511c48c1c71c904d139e06b58cb6027
Parham Alvani <parham.alvani@gmail.com>
2016-10-14 18:07:47 +0330

    Discovery controller was improved :)

--- I1820/controller/discovery.py
+++ I1820/controller/discovery.py
@@ -17,18 +17,24 @@ class DiscoveryController(I1820Controller):
         self.rpis = dict()
 
     def ping(self, message: dict):
+        message['things'] = {tuple(t) for t in message['things']}
+
         if message['rpi_id'] not in self.rpis:
             self.rpis[message['rpi_id']] = {
                 'time': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                'things': []
+                'things': set()
             }
         else:
             self.rpis[message['rpi_id']]['time'] = \
                     datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
-        for thing in message['things']:
-            if not Things.get(thing['type']).has_thing(message['rpi_id'],
-                                                       thing['id']):
-                Things.get(thing['type']).new_thing(
-                    message['rpi_id'], thing['id'])
-                self.rpis[message['rpi_id']]['things'].append(thing)
+        to_be_add = message['things'] - self.rpis[message['rpi_id']]['things']
+        to_be_del = self.rpis[message['rpi_id']]['things'] - message['things']
+
+        for thing in to_be_del:
+            Things.get(thing[0]).del_thing(message['rpi_id'], thing[1])
+            self.rpis[message['rpi_id']]['things'].remove(thing)
+
+        for thing in to_be_add:
+            Things.get(thing[0]).new_thing(message['rpi_id'], thing[1])
+            self.rpis[message['rpi_id']]['things'].add(thing)
