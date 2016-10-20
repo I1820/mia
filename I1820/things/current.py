379e09b0b17c0272cff180b51318ad1d6c7026f5
Parham Alvani <parham.alvani@gmail.com>
2016-10-20 22:21:30 +0330

    Current sensor was added.

---
+++ I1820/things/current.py
@@ -0,0 +1,22 @@
+# In The Name Of God
+# ========================================
+# [] File Name : current.py
+#
+# [] Creation Date : 10-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+from .sensor import SensorThing
+from .types import State
+
+
+class Dummy(SensorThing):
+    """
+    This class represents Current sensor
+    """
+    name = "current"
+
+    current = State()
+
+    def __init__(self, rpi_id, device_id):
+        super().__init__(rpi_id, device_id)
