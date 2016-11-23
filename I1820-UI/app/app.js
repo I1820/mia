01ef9d6a2c92d349fbdd7b76ce54402da327ac27
Parham Alvani <parham.alvani@gmail.com>
2016-11-23 07:18:28 +0330

    Simple angular application was added.

---
+++ I1820-UI/app/app.js
@@ -0,0 +1,16 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 23-11-2016
+ * |
+ * | File Name:     app.js
+ * +===============================================
+ */
+'use strict';
+
+
+angular.module('I1820-UI', [])
+
+.controller('MainCtrl', function ($scope) {
+})
