d7937c895652fb4f2957a3e5e68f40d4363f54b9
Parham Alvani <parham.alvani@gmail.com>
2016-11-24 01:35:14 +0330

    Angular basis were added.

---
+++ I1820-UI/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name i1820UiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the i1820UiApp
+ */
+angular.module('i1820UiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
