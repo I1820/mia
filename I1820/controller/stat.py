f9643f06431c51c97d7b6fa4c7beca863a52ef1a
Parham Alvani <parham.alvani@gmail.com>
2016-11-23 07:19:54 +0330

    Status controller basis was added.

---
+++ I1820/controller/stat.py
@@ -0,0 +1,13 @@
+# In The Name Of God
+# ========================================
+# [] File Name : stat.py
+#
+# [] Creation Date : 22-11-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+from .base import I1820Controller
+
+
+class StatController(I1820Controller):
+    pass
