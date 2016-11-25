21c850ddd732130abf178ac61a42466fb8ed1c8d
Parham Alvani <parham.alvani@gmail.com>
2016-11-25 13:39:20 +0330

    Agent route was added.

--- I1820-UI/app/scripts/app.js
+++ I1820-UI/app/scripts/app.js
@@ -29,6 +29,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/agent/:id', {
+        templateUrl: 'views/agent.html',
+        controller: 'AgentCtrl',
+        controllerAs: 'agent'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ I1820-UI/app/scripts/controllers/agent.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name i1820UiApp.controller:AgentCtrl
+ * @description
+ * # AgentCtrl
+ * Controller of the i1820UiApp
+ */
+angular.module('i1820UiApp')
+  .controller('AgentCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
