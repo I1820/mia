83f95d65043b58454baa4a22f753de8ee401e946
Parham Alvani <parham.alvani@gmail.com>
2016-11-27 03:46:18 +0330

    Logger configuration was added.

---
+++ I1820/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+i1820_log_format = (
+    'I1820: [%(asctime)s] %(levelname)-8s %(name)-12s %(thread)-8s %(message)s'
+)
+
+i1820_log_formatter = logging.Formatter(i1820_log_format)
+i1820_log_handler = logging.StreamHandler()
+i1820_log_handler.setLevel(logging.DEBUG)
+i1820_log_handler.setFormatter(i1820_log_formatter)
+logger = logging.getLogger('I1820')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(i1820_log_handler)
