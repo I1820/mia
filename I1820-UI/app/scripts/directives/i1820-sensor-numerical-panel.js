7fd9a67263fa2437ecd09b89e07d9000d84ec5d9
Parham Alvani <parham.alvani@gmail.com>
2016-11-28 23:55:28 +0330

    Sensor panel directive was completed.

--- I1820-UI/app/scripts/directives/i1820-sensor-numerical-panel.js
+++ I1820-UI/app/scripts/directives/i1820-sensor-numerical-panel.js
@@ -7,12 +7,32 @@
  * # i1820SensorNumericalPanel
  */
 angular.module('i1820UiApp')
-  .directive('i1820SensorNumericalPanel', function () {
+  .directive('i1820SensorNumericalPanel', function ($interval, ModelService, ThingService) {
     return {
-      template: '<div></div>',
+      templateUrl: 'views/templates/sensor-numerical-panel.html',
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the I1820SensorNumericalPanel directive');
+      scope: {
+        agentId: '=',
+        type: '=',
+        thingId: '=',
+      },
+      link: function ($scope) {
+        $scope.states = [];
+
+        ModelService.getModel($scope.type).then(function (data) {
+          $scope.states = data.states;
+        });
+
+        $scope.values = {};
+
+        $scope.refresh = function () {
+          ThingService.getState($scope.agentId, $scope.thingId,
+                                $scope.type, []).then(function (data) {
+            $scope.values = data;
+          });
+        };
+
+        $interval($scope.refresh, 1000);
       }
     };
   });
