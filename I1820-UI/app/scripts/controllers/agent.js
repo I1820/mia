2fa551dfb80d2ea7185ba51cb29c0cb697783ba7
Parham Alvani <parham.alvani@gmail.com>
2016-11-29 00:08:59 +0330

    Agent controller was completed.

--- I1820-UI/app/scripts/controllers/agent.js
+++ I1820-UI/app/scripts/controllers/agent.js
@@ -8,6 +8,7 @@
  * Controller of the i1820UiApp
  */
 angular.module('i1820UiApp')
-  .controller('AgentCtrl', function ($scope, $routeParams) {
+  .controller('AgentCtrl', function ($scope, $routeParams, DiscoveryService) {
     $scope.agentId = $routeParams.agentId;
+    $scope.things = DiscoveryService.query($routeParams.agentId).things;
   });
