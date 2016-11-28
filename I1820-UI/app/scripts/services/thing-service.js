9cfd2cfd83f75dc5b73a72d56794bf3e014546c5
Parham Alvani <parham.alvani@gmail.com>
2016-11-29 00:09:10 +0330

    Thing service was improved.

--- I1820-UI/app/scripts/services/thing-service.js
+++ I1820-UI/app/scripts/services/thing-service.js
@@ -8,22 +8,21 @@
  * Service in the i1820UiApp.
  */
 angular.module('i1820UiApp')
-  .service('ThingService', function ($http, $interval) {
+  .service('ThingService', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    this.getState = function (agentId, deviceId, type, state, retval) {
+    this.getState = function (agentId, thingId, type, states) {
       var msg = {
         'agent_id': agentId,
-        'device_id': deviceId,
+        'device_id': thingId,
         'type': type,
-        'states': [state]
+        'states': states
       };
-      var _getState = function () {
-        $http.post('thing', msg).then(function (response) {
-          retval = response.data[state];
-        });
-      };
-      $interval(_getState, 1000);
+      return $http.post('/thing', msg).then(function (response) {
+        if (typeof response.data === 'object') {
+          return response.data;
+        }
+      });
     };
 
     this.setSetting = function () {
