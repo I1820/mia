a549060df2830b9dcd59cb6be24218f8ec5fb8e2
Parham Alvani <parham.alvani@gmail.com>
2016-11-30 02:15:58 +0330

    Statistic issue was corrected.

--- I1820/things/types.py
+++ I1820/things/types.py
@@ -65,13 +65,18 @@ class Statistic:
         self.storage = {}
 
     def __get__(self, obj, objtype):
-        time = self.storage.get((obj.agent_id, obj.device_id), None)
-        return time.strftime("%Y-%m-%dT%H:%M:%SZ")\
-            if time is not None else None
+        value = self.storage.get((obj.agent_id, obj.device_id), None)
+        if value is not None:
+            result = {}
+            result['time'] = value['time'].strftime("%Y-%m-%dT%H:%M:%SZ")
+            result['value'] = value['value']
+            return result
+        else:
+            return None
 
     def __set__(self, obj, value):
         if isinstance(value, dict):
-            self.storage[(obj.agent_id, obj.device_id)] = value['time']
+            self.storage[(obj.agent_id, obj.device_id)] = value
 
 
 class Setting:
