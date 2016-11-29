79a1f0e5181737f344126af04fd37f4d71582c70
Parham Alvani <parham.alvani@gmail.com>
2016-11-30 02:52:03 +0330

    Socket-io initiation was added.

--- I1820/http/__init__.py
+++ I1820/http/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+import socketio
+
+sio = socketio.Server(async_mode='threading')
 
 app = Flask("I1820")
 app.config['LOGGER_NAME'] = 'I1820.http'
+app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
+
 CORS(app)
