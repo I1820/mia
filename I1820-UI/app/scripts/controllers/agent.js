4bf85a6a33fa3fbbb6af9b5dfd5353f6b4247969
Parham Alvani <parham.alvani@gmail.com>
2016-12-02 00:58:53 +0330

    Agent controller was improved.

--- I1820-UI/app/scripts/controllers/agent.js
+++ I1820-UI/app/scripts/controllers/agent.js
@@ -8,7 +8,12 @@
  * Controller of the i1820UiApp
  */
 angular.module('i1820UiApp')
-  .controller('AgentCtrl', function ($scope, $routeParams, DiscoveryService) {
+  .controller('AgentCtrl', function ($scope, $routeParams, $location, DiscoveryService) {
     $scope.agentId = $routeParams.agentId;
     $scope.things = DiscoveryService.query($routeParams.agentId).things;
+
+    $scope.remove = function () {
+      DiscoveryService.remove($routeParams.agentId);
+      $location.path('/');
+    };
   });
