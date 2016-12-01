49d2461bb1f80b4d954c5e83e11679f410b7f27f
Parham Alvani <parham.alvani@gmail.com>
2016-12-02 00:59:11 +0330

    Provides a uptime with stat controller.

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -2,7 +2,9 @@
 
 from I1820.http.main import main as main_http
 from I1820.mqtt.main import main as main_mqtt
+from I1820.controllers.stat import StatController
 
 if __name__ == '__main__':
+    StatController()
     main_mqtt()
     main_http()
