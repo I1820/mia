cb7fa31bb657e6a0da10b6fd85e1c60086c01980
Parham Alvani <parham.alvani@gmail.com>
2016-12-02 02:02:17 +0330

    Our scenario maker was made even funner :D

--- 18.20-scenario.py
+++ 18.20-scenario.py
@@ -7,12 +7,12 @@ if __name__ == '__main__':
         'type': 'filter',
         'chain': 1,
         'arguments': {
-            'agent_id': '42f6a151-92bb-552d-ba69-bf1d25def01f',
-            'device_id': '1',
-            'type': 'current',
-            'state': 'current',
-            'value': '30',
-            'op': '=='
+            'agent_id': '*',
+            'device_id': '*',
+            'type': '*',
+            'state': 'light',
+            'value': '900',
+            'op': '>'
         }
     }
 
@@ -28,6 +28,70 @@ if __name__ == '__main__':
             'type': 'lamp',
             'settings': {
                 'on': True
+            }
+        }
+    }
+
+    r = requests.post('http://127.0.0.1:8080/plugin', json=n)
+    print(r.text)
+
+    n = {
+        'type': 'notifier',
+        'chain': 1,
+        'arguments': {
+            'agent_id': '42f6a151-92bb-552d-ba69-bf1d25def01f',
+            'device_id': '1:2',
+            'type': 'lamp',
+            'settings': {
+                'on': True
+            }
+        }
+    }
+
+    r = requests.post('http://127.0.0.1:8080/plugin', json=n)
+    print(r.text)
+
+    f = {
+        'type': 'filter',
+        'chain': 2,
+        'arguments': {
+            'agent_id': '*',
+            'device_id': '*',
+            'type': '*',
+            'state': 'light',
+            'value': '900',
+            'op': '<'
+        }
+    }
+
+    r = requests.post('http://127.0.0.1:8080/plugin', json=f)
+    print(r.text)
+
+    n = {
+        'type': 'notifier',
+        'chain': 2,
+        'arguments': {
+            'agent_id': '42f6a151-92bb-552d-ba69-bf1d25def01f',
+            'device_id': '1:1',
+            'type': 'lamp',
+            'settings': {
+                'on': False
+            }
+        }
+    }
+
+    r = requests.post('http://127.0.0.1:8080/plugin', json=n)
+    print(r.text)
+
+    n = {
+        'type': 'notifier',
+        'chain': 2,
+        'arguments': {
+            'agent_id': '42f6a151-92bb-552d-ba69-bf1d25def01f',
+            'device_id': '1:2',
+            'type': 'lamp',
+            'settings': {
+                'on': False
             }
         }
     }
