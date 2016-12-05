ad01972b28d9e3306b5662afdcfbb8ee89e944c3
Parham Alvani <parham.alvani@gmail.com>
2016-12-05 07:09:35 +0330

    Even we don't know what is it !

--- I1820-UI/test/karma.conf.js
+++ I1820-UI/test/karma.conf.js
@@ -29,6 +29,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-socket-io/socket.js',
+      'bower_components/angular-timeago/dist/angular-timeago.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
