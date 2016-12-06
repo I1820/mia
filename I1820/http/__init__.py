eaf0c519d0a88d722d3b884593e1a6baed2c678a
Parham Alvani <parham.alvani@gmail.com>
2016-12-06 16:55:08 +0330

    Uses eventlet for socketIO :)

--- I1820/http/__init__.py
+++ I1820/http/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 import socketio
 
-sio = socketio.Server(async_mode='threading')
+sio = socketio.Server(async_mode='eventlet')
 
 app = Flask("I1820")
 app.config['LOGGER_NAME'] = 'I1820.http'
