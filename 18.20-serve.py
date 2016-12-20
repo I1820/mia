233320766f6360ec913f3fcfbca1b77a16415736
Parham Alvani <parham.alvani@gmail.com>
2016-12-20 08:40:31 +0330

    Deprecation date was corrected.

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -5,7 +5,7 @@ from I1820.mqtt.main import main as main_mqtt
 from I1820.controllers.stat import StatController
 
 if __name__ == '__main__':
-    print(' * 18.20 left us at Sep 06 2016')
+    print(' * 18.20 at Sep 07 2016 7:20 IR721')
     StatController()
     main_mqtt()
     main_http()
