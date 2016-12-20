fb7eb8a20680e61820859cf7c05cbd5df5ed9209
Parham Alvani <parham.alvani@gmail.com>
2016-12-20 08:37:15 +0330

    Removes Logger and eventlet and adds gevent.

--- I1820/http/__init__.py
+++ I1820/http/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 import socketio
 
-sio = socketio.Server(async_mode='eventlet')
+sio = socketio.Server(async_mode='gevent')
 
 app = Flask("I1820")
-app.config['LOGGER_NAME'] = 'I1820.http'
 app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
 
 CORS(app)
