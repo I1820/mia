16684f68acdc1bc71cf3cf6488acb67c1d64fe5d
Parham Alvani <parham.alvani@gmail.com>
2016-12-20 08:35:33 +0330

    Print I1820 left date :)

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -5,6 +5,7 @@ from I1820.mqtt.main import main as main_mqtt
 from I1820.controllers.stat import StatController
 
 if __name__ == '__main__':
+    print(' * 18.20 left us at Sep 06 2016')
     StatController()
     main_mqtt()
     main_http()
