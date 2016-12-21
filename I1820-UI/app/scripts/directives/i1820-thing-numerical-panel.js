d83c333914bfbc0de649e74c019a1a760310bd61
Parham Alvani <parham.alvani@gmail.com>
2016-12-21 09:59:53 +0330

    Beta version of socket.io base notification was added.

--- I1820-UI/app/scripts/directives/i1820-thing-numerical-panel.js
+++ I1820-UI/app/scripts/directives/i1820-thing-numerical-panel.js
@@ -7,7 +7,7 @@
  * # i1820SensorNumericalPanel
  */
 angular.module('i1820UiApp')
-  .directive('i1820ThingNumericalPanel', function ($interval, ModelService, ThingService) {
+  .directive('i1820ThingNumericalPanel', function ($interval, $socket, ModelService, ThingService) {
     return {
       templateUrl: 'views/templates/thing-numerical-panel.html',
       restrict: 'E',
@@ -23,6 +23,26 @@ angular.module('i1820UiApp')
         $scope.events = [];
 
         var stateWatcher;
+        $scope.values = {};
+        var refreshStates = function () {
+          ThingService.getState($scope.agentId, $scope.thingId,
+                                $scope.type, []).then(function (data) {
+            for (var i in data) {
+              $scope.values[i] = data[i];
+            }
+          });
+        };
+
+        var refreshEvents = function (event) {
+          event = JSON.parse(event);
+          if (event['agent_id'] === $scope.agentId &&
+              event['device_id'] === $scope.thingId &&
+                event['type'] === $scope.type) {
+            for (var i in event.state) {
+              $scope.values[i] = event.state[i];
+            }
+          }
+        };
 
         ModelService.getModel($scope.type).then(function (data) {
           if (data.hasOwnProperty('states')) {
@@ -41,20 +61,24 @@ angular.module('i1820UiApp')
           if ($scope.states.length !== 0) {
             stateWatcher = $interval($scope.refresh, 5000);
           }
+
+          if ($scope.events.length !== 0) {
+            $socket.addListener('event', refreshEvents);
+          }
         });
 
         $scope.$on('$destroy', function () {
-          $interval.cancel(stateWatcher);
+          if ($scope.states.length !== 0) {
+            $interval.cancel(stateWatcher);
+          }
+
+          if ($scope.events.length !== 0) {
+            $socket.removeListener('event', refreshEvents);
+          }
         });
 
-        $scope.values = {};
 
-        $scope.refresh = function () {
-          ThingService.getState($scope.agentId, $scope.thingId,
-                                $scope.type, []).then(function (data) {
-            $scope.values = data;
-          });
-        };
+        $scope.refresh = refreshStates;
 
         $scope.turn = function (setting, value) {
           ThingService.setSetting($scope.agentId, $scope.thingId, $scope.type, setting, value);
