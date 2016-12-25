6992dc8dd9ed2decc9289e64c0742a1b71d588fe
Parham Alvani <parham.alvani@gmail.com>
2016-12-25 23:12:07 +0330

    Navigation controller was added.

---
+++ I1820-UI/app/scripts/controllers/nav.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name i1820UiApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the i1820UiApp
+ */
+angular.module('i1820UiApp')
+  .controller('NavCtrl', function ($scope, $location) {
+    $scope.$watch(function () {
+      return $location.path();
+    }, function (path) {
+      if (path.includes('plugin')) {
+        $scope.activeView = 'plugin';
+      } else if (path.includes('about')) {
+        $scope.activeView = 'about';
+      } else {
+        $scope.activeView = 'home';
+      }
+    });
+  });
