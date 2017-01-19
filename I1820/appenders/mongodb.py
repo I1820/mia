4400773b69c45e8f3fe8a48dabe8b1c92f84f5ac
Parham Alvani <parham.alvani@gmail.com>
2017-01-19 03:54:06 +0330

    MongoDB appender was added.

---
+++ I1820/appenders/mongodb.py
@@ -0,0 +1,36 @@
+from .base import I1820LogAppender
+from ..conf.config import cfg
+
+import pymongo
+
+
+class MongodbLogAppender(I1820LogAppender):
+    def __init__(self):
+        self._client = pymongo.MongoClient(host=cfg.appenders_mongodb_host,
+                                           port=cfg.appenders_mognodb_port)
+        self._client = self._client[cfg.appenders_mongodb_db]
+
+    def save(self, measurement, agent_id, device_id, time, value):
+        collection = self._client[measurement]
+        point = {
+            "agent_id": agent_id,
+            "device_id": device_id,
+            "time": time,
+            "value": value
+        }
+        collection.insert_one(point)
+
+    def last(self, measurement, agent_id, device_id):
+        collection = self._client[measurement]
+        q = {
+            "agent_id": agent_id,
+            "device_id": device_id
+        }
+        result = collection.find_one(q, sort=[('time', pymongo.DESCENDING)])
+        if result is None:
+            return {'value': None, 'time': None}
+        else:
+            return {'value': result['value'], 'time': result['time']}
+
+    def since(self, measurement, agent_id, device_id, since, limit=10):
+        pass
