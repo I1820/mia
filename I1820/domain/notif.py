375eab224dcfee5ab2987a507c16c8dcbc28f66c
Parham Alvani <parham.alvani@gmail.com>
2017-01-21 06:20:43 +0330

    Notification domain model was improved.

--- I1820/domain/notif.py
+++ I1820/domain/notif.py
@@ -20,23 +20,29 @@ class I1820Notification:
     :type device: str
     :param settings: configurations wanted to apply on target end device.
     :type settings: dict
-    :param endpoint: identification of target end device Raspberry PI.
-    :type endpoint: str
+    :param agent: identification of target end device agent [Raspberry PI].
+    :type agent: str
     '''
     def __init__(self, type: str, device: str,
-                 settings: dict,
-                 endpoint: str):
+                 settings: list,
+                 agent: str):
+
+        for setting in settings:
+            if 'name' not in setting or 'value' not in setting:
+                raise ValueError(
+                    'settings must be an array of names and values.')
+
         self.type = type
         self.device = device
         self.settings = settings
-        self.endpoint = endpoint
+        self.agent = agent
 
     def to_json(self):
             result = {
                 'type': self.type,
                 'device': self.device,
                 'settings': self.settings,
-                'endpoint': self.endpoint
+                'agent': self.agent
             }
             return json.dumps(result)
 
@@ -47,5 +53,5 @@ class I1820Notification:
         type = raw_values['type']
         device = raw_values['device']
         settings = raw_values['settings']
-        endpoint = raw_values['endpoint']
-        return cls(type, device, settings, endpoint)
+        agent = raw_values['agent']
+        return cls(type, device, settings, agent)
