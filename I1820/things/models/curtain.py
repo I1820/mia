63bfcc4b2691d5424f4189188c409d31a53ea01f
mahtab farrokh <mahtab.farrokh@yahoo.com>
2017-01-28 16:25:02 +0330

    curtain actuator added

---
+++ I1820/things/models/curtain.py
@@ -0,0 +1,17 @@
+
+
+from ..actuator import ActuatorThing
+from ..types import Setting
+
+class Curtain(ActuatorThing):
+	"""
+	This class represents Curtain actuator 
+	"""
+
+	name = "curtain"
+	
+	height  = Setting(type = "integer")	
+		
+	def __init__(self, agent_id , device_id): 
+		super().__init__(agent_id , device_id) 
+
