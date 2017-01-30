1307d984200ae62f5a1155f2074b62defe94bf8e
Parham Alvani <parham.alvani@gmail.com>
2017-01-30 18:34:43 +0330

    Smart lamp model was improved.

--- I1820/things/models/smartLamp.py
+++ I1820/things/models/smartLamp.py
@@ -1,4 +1,4 @@
-# In The Name Of God 
+# In The Name Of God
 # Creation Date : 1/29/17
 # Created By : Mahtab Farrokh (mahtab.farrokh@gmail.com)
 
@@ -6,14 +6,17 @@
 from ..actuator import ActuatorThing
 from ..types import Setting
 
-class SmartLamp (ActuatorThing) :
+
+class SmartLamp (ActuatorThing):
     """
     This class represents smart lamp actuator
     """
 
     name = "smartLamp"
 
-    color = Setting(type = 'string')
+    on = Setting()
+    color = Setting(type='string')
+    fade = Setting(type='integer')
 
     def __init__(self, agent_id, device_id):
         super().__init__(agent_id, device_id)
