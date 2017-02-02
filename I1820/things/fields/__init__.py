2b645a95b59404d55a491cc772aa022bc37f7392
Parham Alvani <parham.alvani@gmail.com>
2017-02-03 02:50:44 +0330

    Module initiation file was corrected.

--- I1820/things/fields/__init__.py
+++ I1820/things/fields/__init__.py
@@ -4,4 +4,4 @@ from .setting import Setting
 from .state import State
 from .base import Field
 
-__all__ = ['event', 'statistic', 'setting', 'state', 'base']
+__all__ = ['Event', 'Statistic', 'Setting', 'State', 'Field']
