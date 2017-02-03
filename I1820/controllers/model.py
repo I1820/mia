ad986756e933c931df978d0adbb650151a09ef9c
Parham Alvani <parham.alvani@gmail.com>
2017-02-03 22:21:17 +0330

    Create model service.

--- I1820/controllers/model.py
+++ I1820/controllers/model.py
@@ -6,14 +6,37 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-from .base import I1820Controller
 from ..things.base import Things
 from ..things.sensor import SensorThing
 from ..things.actuator import ActuatorThing
 
+from pelix.ipopo.decorators import ComponentFactory, Property, Provides, \
+         Validate, Invalidate, Instantiate
 
-class ModelController(I1820Controller):
+
+@ComponentFactory("model_factory")
+@Provides("model_service")
+@Property("default")
+@Instantiate("default_model_instance")
+class ModelService:
     def __init__(self):
+        pass
+
+    @Validate
+    def validate(self, context):
+        """
+        The component is validated. This method is called right before the
+        provided service is registered to the framework.
+        """
+        # All setup should be done here
+        print(" * 18.20 Service: Model Service")
+
+    @Invalidate
+    def invalidate(self, context):
+        """
+        The component has been invalidated. This method is called right after
+        the provided service has been removed from the framework.
+        """
         pass
 
     def get_model(self, thing):
