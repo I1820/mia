e9bf8076f513e56c2e38ce1cfa48827500103cb5
Parham Alvani <parham.alvani@gmail.com>
2017-02-03 18:24:55 +0330

    I1820 framework start phase was added.

--- I1820/controllers/__init__.py
+++ I1820/controllers/__init__.py
@@ -1,8 +1,15 @@
 import pelix.framework
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 # Prepare the framework, with iPOPO
 i1820_framework = pelix.framework.create_framework((
     # iPOPO
     "pelix.ipopo.core")
 )
+
+# Start the framework, and the pre-installed bundles
+logger.info('I1820 Framework was started :P')
+i1820_framework.start()
