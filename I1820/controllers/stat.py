a8cf9b26f9eac3655e5c7c0f4ec5af999fb5d984
Parham Alvani <parham.alvani@gmail.com>
2017-02-03 21:21:26 +0330

    Stat service was created.

--- I1820/controllers/stat.py
+++ I1820/controllers/stat.py
@@ -6,14 +6,37 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-from .base import I1820Controller
-
 import datetime
 
+from pelix.ipopo.decorators import ComponentFactory, Property, Provides, \
+         Validate, Invalidate, Instantiate
 
-class StatController(I1820Controller):
+
+@ComponentFactory("stat_factory")
+@Provides("stat_service")
+@Property("stat")
+@Instantiate("stat_instance")
+class StatService:
     def __init__(self):
+        self.start_time = None
+
+    @Validate
+    def validate(self, context):
+        """
+        The component is validated. This method is called right before the
+        provided service is registered to the framework.
+        """
+        # All setup should be done here
+        print(" * 18.20 Service: Stat Service")
         self.start_time = datetime.datetime.now()
+
+    @Invalidate
+    def invalidate(self, context):
+        """
+        The component has been invalidated. This method is called right after
+        the provided service has been removed from the framework.
+        """
+        self.start_time = None
 
     def uptime(self):
         return datetime.datetime.now() - self.start_time
