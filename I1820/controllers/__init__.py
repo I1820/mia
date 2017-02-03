9f68792b7eb5821b00d322270c221b5d88f24d39
Parham Alvani <parham.alvani@gmail.com>
2017-02-03 21:21:03 +0330

    Framework context was created.

--- I1820/controllers/__init__.py
+++ I1820/controllers/__init__.py
@@ -12,3 +12,12 @@ i1820_framework = pelix.framework.create_framework(
 # Start the framework, and the pre-installed bundles
 print(" * I1820 Framework is ready")
 i1820_framework.start()
+
+# Get the bundle context of the framework, i.e. the link between the
+# framework starter and its content.
+i1820_framework_context = i1820_framework.get_bundle_context()
+
+
+# Service Initiation
+# Stat service
+i1820_framework_context.install_bundle("I1820.controllers.stat").start()
