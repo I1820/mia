6e53076d9a3067288edb6bdaf06ae5c1fd0a4798
Parham Alvani <parham.alvani@gmail.com>
2017-02-03 21:05:35 +0330

    Stat controller initiation was removed.

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -2,10 +2,8 @@
 
 from I1820.http.main import main as main_http
 from I1820.mqtt.main import main as main_mqtt
-from I1820.controllers.stat import StatController
 
 if __name__ == '__main__':
     print(' * 18.20 at Sep 07 2016 7:20 IR721')
-    StatController()
     main_mqtt()
     main_http()
