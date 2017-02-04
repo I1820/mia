1d6165e4d12027cb38d2d34c6276b31626beda3c
Parham Alvani <parham.alvani@gmail.com>
2017-02-04 04:08:47 +0330

    controllers initiation was added.

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -2,8 +2,10 @@
 
 from I1820.http.main import main as main_http
 from I1820.mqtt.main import main as main_mqtt
+from I1820.controllers.main import main as main_cntl
 
 if __name__ == '__main__':
     print(' * 18.20 at Sep 07 2016 7:20 IR721')
+    main_cntl()
     main_mqtt()
     main_http()
