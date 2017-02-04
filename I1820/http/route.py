ecb848011d3c179724c3f060a57e88c6c867ba2e
Parham Alvani <parham.alvani@gmail.com>
2017-02-04 07:24:31 +0330

    Using stat and model services in http routes.

--- I1820/http/route.py
+++ I1820/http/route.py
@@ -12,10 +12,10 @@ import json
 import jsonschema
 
 from . import app
+from pelix.utilities import use_service
 from ..domain.schemas.schema import log_request_schema
 from ..things.base import Things
-from ..controllers.stat import StatController
-from ..controllers.model import ModelController
+from ..controllers import i1820_framework_context
 from ..controllers.discovery import DiscoveryController
 from ..controllers.plugin import PluginController
 from ..exceptions.thing import \
@@ -49,7 +49,11 @@ def root_handler():
 
 @app.route('/model/<string:thing>', methods=['GET'])
 def model_handler(thing):
-    return json.dumps(ModelController().get_model(thing))
+    model_service_ref = i1820_framework_context.get_service_reference(
+        "model_service")
+    with use_service(i1820_framework_context, model_service_ref) \
+            as model_service:
+        return json.dumps(model_service.get_model(thing))
 
 
 # Agents
@@ -150,7 +154,11 @@ def plugin_list_handler():
 
 @app.route('/stat/uptime', methods=['GET'])
 def stat_uptime_handler():
-    return str(StatController().uptime())
+    stat_service_ref = i1820_framework_context.get_service_reference(
+        "stat_service")
+    with use_service(i1820_framework_context, stat_service_ref) \
+            as stat_service:
+        return str(stat_service.uptime())
 
 
 # Error Side :P
