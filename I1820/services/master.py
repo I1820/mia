14720a50c44d7d48740495c8eb82646b73cd6d7e
Parham Alvani <parham.alvani@gmail.com>
2017-02-04 20:51:23 +0330

    Service master issues were corrected.

--- I1820/services/master.py
+++ I1820/services/master.py
@@ -1,6 +1,7 @@
 import pelix.framework
 from pelix.utilities import use_service
 
+
 class ServiceMaster:
     def __init__(self):
         self.i1820_framework = None
@@ -12,15 +13,18 @@ class ServiceMaster:
         )
         print(" * 18.20 Service Framework >>")
         self.i1820_framework.start()
-        self.i1820_framework_context = self.i1820_framework.get_bundle_context()
-        self.i1820_framework_context.install_bundle("I1820.services.stat").start()
-        self.i1820_framework_context.install_bundle("I1820.services.model").start()
+        self.i1820_framework_context = \
+            self.i1820_framework.get_bundle_context()
+        self.i1820_framework_context.install_bundle(
+            "I1820.services.stat").start()
+        self.i1820_framework_context.install_bundle(
+            "I1820.services.model").start()
         print(" * 18.20 Service Framework <<")
 
     def service(self, service_name):
         service_ref = self.i1820_framework_context.get_service_reference(
             service_name)
         return use_service(self.i1820_framework_context, service_ref)
- 
+
 
 service_master = ServiceMaster()
