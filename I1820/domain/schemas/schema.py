5ab77d60f39bba0eb47c303353b0bccda9d3fe2a
Parham Alvani <parham.alvani@gmail.com>
2017-02-11 20:22:43 +0330

    Notification loading was added.

--- I1820/domain/schemas/schema.py
+++ I1820/domain/schemas/schema.py
@@ -5,3 +5,5 @@ log_schema = json.load(open(os.path.join(os.path.dirname(__file__),
                                          "log.json")))
 log_request_schema = json.load(open(os.path.join(os.path.dirname(__file__),
                                                  "log-request.json")))
+notif_request_schema = json.load(open(os.path.join(os.path.dirname(__file__),
+                                                   "notif-request.json")))
