d0afdf7a8f2c4988b86870d7c50153f32fbe9f8b
Parham Alvani <parham.alvani@gmail.com>
2017-02-14 10:01:33 +0330

    Provides exit handling for i1820 :D

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -3,9 +3,14 @@
 from I1820.http.main import main as main_http
 from I1820.mqtt.main import main as main_mqtt
 from I1820.services.main import main as main_srv
+from I1820.services.main import die as die_srv
 
 if __name__ == '__main__':
     print(' * 18.20 at Sep 07 2016 7:20 IR721')
     main_srv()
     main_mqtt()
-    main_http()
+    try:
+        main_http()
+    except KeyboardInterrupt:
+        print(" > 18.20 As always ... left me alone")
+        die_srv()
