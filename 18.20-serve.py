2f2501dc5fcbb7e53225d3e65759a771de0479c8
Parham Alvani <parham.alvani@gmail.com>
2017-02-14 08:50:45 +0330

    Redis main was removed.

--- 18.20-serve.py
+++ 18.20-serve.py
@@ -2,12 +2,10 @@
 
 from I1820.http.main import main as main_http
 from I1820.mqtt.main import main as main_mqtt
-from I1820.redis.main import main as main_redis
 from I1820.services.main import main as main_srv
 
 if __name__ == '__main__':
     print(' * 18.20 at Sep 07 2016 7:20 IR721')
     main_srv()
-    main_redis()
     main_mqtt()
     main_http()
