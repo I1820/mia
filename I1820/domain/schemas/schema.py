3602839e43d33fec81ac3285de69c27afa0a7280
Parham Alvani <parham.alvani@gmail.com>
2017-02-24 07:12:20 +0330

    Agent schema was loaded.

--- I1820/domain/schemas/schema.py
+++ I1820/domain/schemas/schema.py
@@ -7,3 +7,5 @@ log_request_schema = json.load(open(os.path.join(os.path.dirname(__file__),
                                                  "log-request.json")))
 notif_request_schema = json.load(open(os.path.join(os.path.dirname(__file__),
                                                    "notif-request.json")))
+agent_schema = json.load(open(os.path.join(os.path.dirname(__file__),
+                                           "agent.json")))
