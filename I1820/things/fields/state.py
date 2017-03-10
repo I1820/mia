9bb193b1573ba1f721281d6b3b2906497652bdc4
Parham Alvani <parham.alvani@gmail.com>
2017-03-10 11:37:13 +0330

    Removes data caching from state field.

--- I1820/things/fields/state.py
+++ I1820/things/fields/state.py
@@ -8,7 +8,6 @@ class State(Field):
 
     def __init__(self):
         super().__init__()
-        self.storage = {}
 
     def __get__(self, obj, objtype):
         value = LogController().last(
@@ -17,9 +16,6 @@ class State(Field):
 
     def __set__(self, obj, value):
         if isinstance(value, dict):
-            # TODO: checks last cached value with new value
-            # caches last value for renewing instead of inserting
-            self.storage[(obj.agent_id, obj.device_id)] = value
             LogController().save(self.name, obj.agent_id,
                                  obj.device_id,
                                  value['time'], value['value'])
