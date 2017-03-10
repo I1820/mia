b443404de57767400e72db846a0fce5567b9bb99
Parham Alvani <parham.alvani@gmail.com>
2017-03-10 21:26:44 +0330

    Uses log service for database communications.

--- I1820/things/fields/state.py
+++ I1820/things/fields/state.py
@@ -1,5 +1,13 @@
+# In The Name Of God
+# ========================================
+# [] File Name : state.py
+#
+# [] Creation Date : 10-03-2017
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
 from ...exceptions.thing import ThingInvalidAccessException
-from ...controllers.log import LogController
+from ...services.master import service_master
 from .base import Field
 
 
@@ -10,13 +18,15 @@ class State(Field):
         super().__init__()
 
     def __get__(self, obj, objtype):
-        value = LogController().last(
-            self.name, obj.agent_id, obj.device_id)
+        with service_master.service('log_service') as log_service:
+            value = log_service.last(
+                self.name, obj.agent_id, obj.device_id)
         return value
 
     def __set__(self, obj, value):
         if isinstance(value, dict):
-            LogController().save(self.name, obj.agent_id,
+            with service_master.service('log_service') as log_service:
+                log_service.save(self.name, obj.agent_id,
                                  obj.device_id,
                                  value['time'], value['value'])
             return
