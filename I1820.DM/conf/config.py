1264e420dcb5bfe6ab4d30937888ed0cd0f619d3
Parham Alvani <parham.alvani@gmail.com>
2017-03-15 22:56:00 +0330

    Configuration of I1820.DM was added.

---
+++ I1820.DM/conf/config.py
@@ -0,0 +1,36 @@
+# In The Name Of God
+# ========================================
+# [] File Name : config.py
+#
+# [] Creation Date : 01-09-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+import os
+import configparser
+
+
+class I1820Config:
+    def __init__(self, path):
+        cfg = configparser.ConfigParser()
+
+        # File based configurations
+        try:
+            cfg.read_file(open(path))
+        except FileNotFoundError:
+            print(" :| we need some configuration as you may know ...")
+            exit()
+
+        self.cfg = cfg
+
+    def __getattr__(self, name):
+        if '_' in name:
+            section, field = name.split('_', maxsplit=1)
+        else:
+            section = name
+        if section == 'mqtt':
+            return self.cfg['mqtt.i1820.org'][field]
+
+
+I1820_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "1820.ini")
+cfg = I1820Config(I1820_CONFIG_PATH)
