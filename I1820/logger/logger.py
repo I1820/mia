c90502a5a9ce1e92654bb1ef00b88a1c3a4d7d6c
Parham Alvani <parham.alvani@gmail.com>
2022-05-15 00:38:02 +0430

    feat: use rich for logging

--- I1820/logger/logger.py
+++ I1820/logger/logger.py
@@ -1,13 +1,17 @@
 import logging
 
-i1820_log_format = (
+from rich.logging import RichHandler
+
+FORMAT = (
     'I1820: [%(asctime)s] %(levelname)-8s %(name)-12s %(thread)-8s %(message)s'
 )
 
-i1820_log_formatter = logging.Formatter(i1820_log_format)
-i1820_log_handler = logging.StreamHandler()
-i1820_log_handler.setLevel(logging.DEBUG)
-i1820_log_handler.setFormatter(i1820_log_formatter)
-logger = logging.getLogger('I1820')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(i1820_log_handler)
+
+def setup():
+    '''
+    setup logging based on rich library and a custom format
+    '''
+    logging.basicConfig(
+        level="NOTSET", format=FORMAT, datefmt="[%X]",
+        handlers=[RichHandler(markup=True)]
+    )
