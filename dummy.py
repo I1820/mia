5041382c1e020ce6e59548366b7d4bda9e1c1b95
Parham Alvani <parham.alvani@gmail.com>
2022-05-18 00:13:53 +0430

    feat: update dummy agent

--- dummy.py
+++ dummy.py
@@ -15,13 +15,12 @@ client = mqtt.Client()
 logger = logging.getLogger('mia.dummy')
 
 
-def ping(ident: int):
+def ping():
     '''
     ping simulate an agent with the given id and hard-coded thing.
     '''
-    logger.info('id = %d', ident)
     agent = Agent('dummy', things={
-                      RawThing(type='dummy', id=str(ident))
+                      RawThing(type='dummy', id='0')
                   }, actions=[])
     client.publish(f'I1820/{TENANT_ID}/agent/ping', agent.to_json())
 
@@ -29,13 +28,13 @@ def ping(ident: int):
 if __name__ == '__main__':
     client.connect('127.0.0.1', int(1883))
     client.loop_start()
-    i = 0
+    i = 2
     while True:
-        i = (i + 1) % 2
-        ping(i)
+        i = (i ** 2) % 1024
+        ping()
         log = I1820Log(type='dummy', device='0', agent='dummy',
                        states=[
-                           {'name': 'chert', 'value': '1'}
+                           {'name': 'chert', 'value': str(i)}
                        ])
         client.publish(f'I1820/{TENANT_ID}/log/send', log.to_json())
         time.sleep(1)
