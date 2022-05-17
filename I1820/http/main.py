421c1c8ed206c4b3b0dedab909db7fae4c69feee
Parham Alvani <parham.alvani@gmail.com>
2022-05-17 21:37:06 +0430

    feat: complete http module registration

--- I1820/http/main.py
+++ I1820/http/main.py
@@ -6,6 +6,7 @@ from ..discovery import DiscoveryService
 from .agent import AgentHanlder
 from .health import HealthHanlder
 from .model import ModelHanlder
+from .thing import ThingHandler
 
 
 def app(ds: DiscoveryService) -> sanic.Sanic:
@@ -16,5 +17,6 @@ def app(ds: DiscoveryService) -> sanic.Sanic:
     app.blueprint(ModelHanlder().register())
     app.blueprint(HealthHanlder().register())
     app.blueprint(AgentHanlder().register())
+    app.blueprint(ThingHandler().register())
 
     return app
