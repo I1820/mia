fd1ec320ad351f3abcdeb29c3d4603e06a31776c
Parham Alvani <parham.alvani@gmail.com>
2022-05-17 08:50:21 +0430

    feat: wrap everything up

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from rich import pretty
 from rich.console import Console
 
 import I1820.conf
+import I1820.discovery
+import I1820.mqtt
 
 if __name__ == '__main__':
     console = Console()
@@ -13,5 +15,9 @@ if __name__ == '__main__':
 
     cfg = I1820.conf.load()
     pretty.pprint(cfg)
+
+    discovery_service = I1820.discovery.DiscoveryService()
+    mqtt_service = I1820.mqtt.MQTTService(cfg.mqtt.host, cfg.mqtt.port,
+                                          cfg.tenant, discovery_service)
 
     console.print("Mia is up and running", style="bold red")
