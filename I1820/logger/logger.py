a0fabf3d9cfa04e9d7e56299236675a3b928d25b
Parham Alvani <parham.alvani@gmail.com>
2022-08-25 12:45:52 +0430

    feat: set log level

--- I1820/logger/logger.py
+++ I1820/logger/logger.py
@@ -4,11 +4,12 @@ from rich.logging import RichHandler
 
 logger = logging.getLogger(__name__)
 
-format = logging.Formatter(
+formatter = logging.Formatter(
     "mia: [%(asctime)s] %(levelname)-8s %(name)-12s %(thread)-8s %(message)s"
 )
 
 handler = RichHandler(markup=True)
-handler.setFormatter(format)
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
 
 logger.addHandler(handler)
