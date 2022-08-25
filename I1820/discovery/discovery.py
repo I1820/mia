9738659af6d18132cabb570c74466ed275e6d666
Parham Alvani <parham.alvani@gmail.com>
2022-08-25 12:45:03 +0430

    feat: add logs into discovery module

--- I1820/discovery/discovery.py
+++ I1820/discovery/discovery.py
@@ -1,14 +1,16 @@
 from datetime import datetime
 from threading import Lock
 
-from ..domain.agent import Agent, RawThing
-from ..things.base import Things
+from I1820.domain.agent import Agent, RawThing
+from I1820.things.base import Things
+from I1820.logger import logger
 
 
 class DiscoveryService:
-    '''
+    """
     DiscoveryService manages agents. agent may have multiple things.
-    '''
+    """
+
     def __init__(self):
         self._agents: dict[str, Agent] = {}
         self.lck = Lock()
@@ -16,29 +18,31 @@ class DiscoveryService:
 
     @property
     def agents(self) -> dict[str, dict]:
-        '''
-        retrieves agent information from redis.
-        '''
+        """
+        retrieves agent information from memory.
+        """
         result: dict[str, dict] = {}
 
         with self.lck:
             for agent_id, agent in self._agents.items():
                 result[agent_id] = {}
 
-                result[agent_id]['time'] = agent.last_seen\
-                    .strftime('%Y-%m-%dT%H:%M:%SZ')
+                result[agent_id]["time"] = agent.last_seen.strftime(
+                    "%Y-%m-%dT%H:%M:%SZ"
+                )
 
-                result[agent_id]['things'] = []
+                result[agent_id]["things"] = []
                 for thg in agent.things:
-                    result[agent_id]['things'].append(
-                        {'type': thg.type, 'id': thg.id})
+                    result[agent_id]["things"].append(
+                        {"type": thg.type, "id": thg.id}
+                    )
         return result
 
     def ping(self, agent: Agent):
-        '''
+        """
         agent pings mia platform, this method saves their status and things.
         for saving things we are using Thing class devices field.
-        '''
+        """
         # Let's create our agent
         to_add: set[RawThing] = set()
         to_del: set[RawThing] = set()
@@ -58,15 +62,17 @@ class DiscoveryService:
         # Add things into local things storage
         for thg in to_add:
             Things.get(thg.type).new_thing(agent.ident, thg.id)
+            logger.info("thing %s was registered", thg.id)
 
         # Remove things from local things storage
         for thg in to_del:
             Things.get(thg.type).del_thing(agent.ident, thg.id)
+            logger.info("thing %s was removed", thg.id)
 
     def pong(self, agent_id: str):
-        '''
+        """
         Removes given agent (based on it's identification) from agent storage.
-        '''
+        """
         with self.lck:
             if agent_id in self._agents:
                 result = 1
